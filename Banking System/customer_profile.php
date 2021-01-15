03ade3a34875705501c6d91bb42d00e0a5be9c19
jacquelineannlim <75509416+jacquelineannlim@users.noreply.github.com>
2021-01-15 11:43:27 +1100

    Update customer_profile.php

--- Banking System/customer_profile.php
+++ Banking System/customer_profile.php
@@ -11,24 +11,28 @@
 </head>
 <body id = "customer_profile_page">
 
-<ul id="navbar">
-    <li style="float: left; padding: 14px 80px; font-size: 26px;"><b>TSF Bank</b></li>
-    <li><a href="transaction_history.php">Transaction History</a></li>
-    <li><a href="view_all_customers.php">View All Customers</a></li>
-    <li><a href="index.php">Home</a></li>
-</ul>
+    <!--Navigation bar -->
+    <ul id="navbar">
+        <li style="float: left; padding: 14px 80px; font-size: 26px;"><b>TSF Bank</b></li>
+        <li><a href="transaction_history.php">Transaction History</a></li>
+        <li><a href="view_all_customers.php">View All Customers</a></li>
+        <li><a href="index.php">Home</a></li>
+    </ul>
 
-<?php
-    $sql = "SELECT * FROM customer_details;";
-    $result = mysqli_query($conn, $sql);
-    $resultCheck = mysqli_num_rows($result);
-    
-    if($resultCheck > 0)
-    {
-        while ($row = mysqli_fetch_assoc($result))
+    <!--Select and view ONE customer's details -->
+    <?php
+        //the selected customer is based on the value of "view" from the form with the POST method (located in the "view_all_customers.php file) 
+        //use customer id to select and identify a customer uniquely
+        $selected_customer = intval($_POST["view"]); 
+        $sql = "SELECT * FROM customer_details WHERE cust_id = $selected_customer;";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
+        
+        if($resultCheck > 0)
         {
-            if($row['cust_id'] == $_POST["view"])
+            while ($row = mysqli_fetch_assoc($result))
             {
+    
                 $customerID = $row['cust_id'];
                 $customerName = $row['cust_name'];
                 $email = $row['email'];
@@ -56,10 +60,10 @@
                         <button class="button_transfer button_transfer2 button" name="transfer" value='.$customerID.'>Transfer Money</button>
                       </form>
                       ';
+                    //The value of the "transfer" button will be sent to "transfer_money.php" by means of 'method' and 'action' form attribute
             }
         }
-    }
-?>
+    ?>
 
 </body>
 </html>
